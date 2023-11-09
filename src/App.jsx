a24008495c24980e47e6a75993bdbbac9be78f6e
NailRz <nailrzr@gmail.com>
2023-11-09 22:09:39 +0300

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,150 +1,182 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
+import data from "./words.json";
 
 function App() {
-  const [inputText, setInputText] = useState("");
-  const words = "ведь идти ребенок сам что даже ну молчать подумать хотеть сразу это просто хочет тут новый без сам видеть вы через в город свой чужой как быть мне с этим делом что могу сделать если";
-  const inputRef = useRef(null);
-  const [startTime, setStartTime] = useState(0);
-  const [endTime, setEndTime] = useState(0);
-  const [wordCount, setWordCount] = useState(0);
-  const [wpm, setWpm] = useState(0);
-  const [isTestComplete, setIsTestComplete] = useState(false);
-  const [timeLeft, setTimeLeft] = useState(0); // Время в секундах
-  const [isTyping, setIsTyping] = useState(false);
+	const [inputText, setInputText] = useState("");
+	const [words, setWords] = useState([]);
+	const inputRef = useRef(null);
+	const [startTime, setStartTime] = useState(0);
+	const [endTime, setEndTime] = useState(0);
+	const [wordCount, setWordCount] = useState(0);
+	const [wpm, setWpm] = useState(0);
+	const [isTestComplete, setIsTestComplete] = useState(false);
+	const [timeLeft, setTimeLeft] = useState(0); // Время в секундах
+	const [isTyping, setIsTyping] = useState(false);
 
-  const startTest = () => {
-    if (inputText.length === 0 && !isTestComplete) {
-      setStartTime(Date.now());
-      setIsTestComplete(false);
-      setTimeLeft(15); // Установка времени в 15 секунд
-      inputRef.current.focus();
-    }
-  };
+	useEffect(() => {
+		// Случайная перестановка массива слов
+		const shuffledWords = data.words.sort(() => Math.random() - 0.5);
+		setWords(shuffledWords);
+	}, []);
 
-  useEffect(() => {
-    const handleKeyPress = (event) => {
-      if (!isTyping) {
-        startTest();
-        setIsTyping(true);
-      }
-    };
+	const startTest = () => {
+		if (inputText.length === 0 && !isTestComplete) {
+			setStartTime(Date.now());
+			setIsTestComplete(false);
+			setTimeLeft(5);
+			inputRef.current.focus();
+		}
+	};
 
-    window.addEventListener('keypress', handleKeyPress);
+	useEffect(() => {
+		const handleKeyPress = () => {
+			if (!isTyping) {
+				startTest();
+				setIsTyping(true);
+			}
+		};
 
-    return () => {
-      window.removeEventListener('keypress', handleKeyPress);
-    };
-  }, [isTyping]);
+		window.addEventListener("keypress", handleKeyPress);
 
-  useEffect(() => {
-    let timer;
-    if (timeLeft > 0 && !isTestComplete) {
-      timer = setTimeout(() => {
-        setTimeLeft((prevTime) => prevTime - 1);
-      }, 1000); // Каждую секунду
-    } else if (timeLeft === 0) {
-      setEndTime(Date.now());
-      setIsTestComplete(true);
-    }
+		return () => {
+			window.removeEventListener("keypress", handleKeyPress);
+		};
+	}, [isTyping]);
 
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [timeLeft, isTestComplete]);
+	useEffect(() => {
+		let timer;
+		if (timeLeft > 0 && !isTestComplete) {
+			timer = setTimeout(() => {
+				setTimeLeft((prevTime) => prevTime - 1);
+			}, 1000); // Каждую секунду
+		} else if (timeLeft === 0) {
+			setEndTime(Date.now());
+			setIsTestComplete(true);
+		}
 
-  const handleInputChange = (e) => {
-    const userInput = e.target.value;
-    setInputText(userInput);
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [timeLeft, isTestComplete]);
 
-    if (userInput.trim().split(/\s+/).length > 0) {
-      setWordCount(userInput.trim().split(/\s+/).length);
-    }
+	const handleInputChange = (e) => {
+		const userInput = e.target.value;
+		setInputText(userInput);
 
-    if (userInput.length === words.length) {
-      setEndTime(Date.now());
-      setIsTestComplete(true);
-    } else {
-      setEndTime(0);
-      setIsTestComplete(false);
-    }
-  };
+		if (userInput.trim().split(/\s+/).length > 0) {
+			setWordCount(userInput.trim().split(/\s+/).length);
+		}
 
-  const calculateWPM = () => {
-    const timeElapsed = (endTime - startTime) / 1000 / 60;
-    const charactersTyped = inputText.length;
-    const calculatedWpm = charactersTyped / 5 / timeElapsed;
-    setWpm(calculatedWpm);
-  };
+		if (userInput.length === words.length) {
+			setEndTime(Date.now());
+			setIsTestComplete(true);
+		} else {
+			setEndTime(0);
+			setIsTestComplete(false);
+		}
+	};
 
-  useEffect(() => {
-    if (isTestComplete && endTime > 0) {
-      calculateWPM();
-    }
-  }, [endTime, isTestComplete]);
+	const calculateWPM = () => {
+		const timeElapsed = (endTime - startTime) / 1000 / 60;
+		const charactersTyped = inputText.length;
+		const calculatedWpm = charactersTyped / 5 / timeElapsed;
+		setWpm(calculatedWpm);
+	};
 
-  const formattedWpm = Number.isFinite(wpm) ? wpm.toFixed(0) : "Calculating...";
+	useEffect(() => {
+		if (isTestComplete && endTime > 0) {
+			calculateWPM();
+		}
+	}, [endTime, isTestComplete]);
 
-  const renderWords = () => {
-    const inputArray = inputText.split("");
-    const wordsArray = words.split("");
+	const formattedWpm = Number.isFinite(wpm) ? wpm.toFixed(0) : "Calculating...";
 
-    return wordsArray.map((letter, index) => {
-      let textColor = "white";
+	const renderWords = () => {
+		const inputArray = inputText.split(/\s+/);
+		return words.map((word, wordIndex) => {
+			let wordLetters = word.split("");
+			let enteredWord = inputArray[wordIndex] || "";
 
-      if (inputArray[index]) {
-        if (letter === inputArray[index]) {
-          textColor = "green";
-        } else {
-          textColor = "red";
-        }
-      }
+			return (
+				<div
+					key={wordIndex} // Добавили ключ к внешнему элементу
+					style={{
+						background: "gray",
+						display: "inline-block",
+						marginInline: "3px",
+					}}
+				>
+					{wordLetters.map((letter, letterIndex) => {
+						let inputLetter = enteredWord[letterIndex];
 
-      return (
-        <span
-          key={index}
-          style={{ color: textColor, cursor: "text" }}
-          onClick={startTest}
-        >
-          {letter}
-        </span>
-      );
-    });
-  };
+						let letterColor = "white";
 
-  return (
-    <>
-      <div style={{ fontFamily: "Arial", fontSize: "18px", lineHeight: "24px" }}>
-        <div style={{ marginBottom: "10px" }}>{renderWords()}</div>
-        <input
-          type="text"
-          ref={inputRef}
-          value={inputText}
-          onChange={handleInputChange}
-          style={{
-            fontSize: "18px",
-            padding: "5px",
-            width: "1px",
-            height: "1px",
-            position: "absolute",
-            top: "-100px",
-            left: "-100px",
-            opacity: 0,
-            overflow: "hidden",
-          }}
-          // style={{
-          //   fontSize: "18px",
-          //   padding: "5px",
-          //   width: "90%",
-          //   maxWidth: "500px",
-          //   margin: "0 auto",
-          // }}
-        />
-        <p>Time Left: {timeLeft} seconds</p>
-        <p>Words per Minute (WPM): {formattedWpm}</p>
-      </div>
-    </>
-  );
+						if (inputLetter !== undefined) {
+							if (inputLetter === letter) {
+								letterColor = "green";
+							} else {
+								letterColor = "red";
+							}
+						}
+
+						return (
+							<span
+								key={letterIndex} // Добавили ключ к каждому <span>
+								style={{
+									color: letterColor,
+									// cursor: "text",
+								}}
+							>
+								{letter}
+							</span>
+						);
+					})}
+					{enteredWord.length > word.length && (
+						<span
+							key="extra" // Добавили ключ к элементу с дополнительными буквами
+							style={{
+								color: "red",
+								fontSize: "16px",
+								fontWeight: "bold",
+							}}
+						>
+							{enteredWord.slice(word.length)}
+						</span>
+					)}
+				</div>
+			);
+		});
+	};
+
+	return (
+		<>
+			<div
+				style={{ fontFamily: "Arial", fontSize: "18px", lineHeight: "24px" }}
+			>
+				<div style={{ marginBottom: "10px" }}>{renderWords()}</div>
+				<input
+					type="text"
+					ref={inputRef}
+					value={inputText}
+					onChange={handleInputChange}
+					style={{
+						fontSize: "18px",
+						padding: "5px",
+						width: "1px",
+						height: "1px",
+						position: "absolute",
+						top: "-100px",
+						left: "-100px",
+						opacity: 0,
+						overflow: "hidden",
+					}}
+				/>
+				<p>Time Left: {timeLeft} seconds</p>
+				<p>Words per Minute (WPM): {formattedWpm}</p>
+			</div>
+		</>
+	);
 }
 
 export default App;
